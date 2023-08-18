5eb158b1b164c323129fc93b1bcbdc6ff7ef2e6e
Yannick Marcon <yannick.marcon@obiba.org>
2023-08-18 09:35:18 +0200

    feat: encrypt local storage with secure-ls

--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -1,7 +1,19 @@
 import { createPinia } from "pinia";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { createPersistedState } from "pinia-plugin-persistedstate";
+import SecureLS from "secure-ls";
 
 const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
+pinia.use(
+  createPersistedState({
+    storage: {
+      getItem: (key) => {
+        return new SecureLS({ isCompression: false }).get(key);
+      },
+      setItem: (key, value) => {
+        new SecureLS({ isCompression: false }).set(key, value);
+      },
+    },
+  })
+);
 
 export { pinia };
