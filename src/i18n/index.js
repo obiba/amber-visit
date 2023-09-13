28af87b6eb0d0d59edc8d84f5c5045a9e736e05c
Yannick Marcon <yannick.marcon@obiba.org>
2023-09-13 22:10:17 +0200

    feat: many languages

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,27 @@
 import en from "./en";
 import fr from "./fr";
+import da from "./da";
+import sv from "./sv";
+import et from "./et";
+import nl from "./nl";
+import pt from "./pt";
+import de from "./de";
+import it from "./it";
+import fi from "./fi";
+import ru from "./ru";
+import el from "./el";
 
 export default {
   en: en,
   fr: fr,
+  da: da,
+  sv: sv,
+  et: et,
+  nl: nl,
+  pt: pt,
+  de: de,
+  it: it,
+  fi: fi,
+  ru: ru,
+  el: el,
 };
