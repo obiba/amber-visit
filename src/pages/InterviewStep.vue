84176349219010484a8d42b82980767a5cd5e9dc
Yannick Marcon <yannick.marcon@obiba.org>
2023-09-29 17:36:56 +0200

    show toc if more than one entry

--- src/pages/InterviewStep.vue
+++ src/pages/InterviewStep.vue
@@ -139,7 +139,7 @@
         />
         <q-separator dark vertical v-if="mode === 'single'" />
         <q-btn-dropdown
-          v-if="mode === 'single' && toc.length > 0"
+          v-if="mode === 'single' && toc.length > 1"
           stretch
           flat
           icon="toc"
